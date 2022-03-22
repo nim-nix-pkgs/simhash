57e174584aea8c997c28f00855ea422de8d4e488
hugosenari <hugosenari@gmail.com>
2022-03-22 00:37:29 -0300

    chore: re-index ../nimpkgs/s/simhash

--- master/flake.nix
+++ master/flake.nix
@@ -9,13 +9,14 @@
   
   inputs.src-simhash-master.flake = false;
   inputs.src-simhash-master.owner = "bung87";
-  inputs.src-simhash-master.ref   = "refs/heads/master";
+  inputs.src-simhash-master.ref   = "master";
   inputs.src-simhash-master.repo  = "simhash-nim";
   inputs.src-simhash-master.type  = "github";
   
   inputs."lc".owner = "nim-nix-pkgs";
   inputs."lc".ref   = "master";
   inputs."lc".repo  = "lc";
+  inputs."lc".dir   = "v0_9_0";
   inputs."lc".type  = "github";
   inputs."lc".inputs.nixpkgs.follows = "nixpkgs";
   inputs."lc".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
